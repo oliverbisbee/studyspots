807d14bc7a8be4c1b34e7b310936c5e47306ae77
oliverbisbee <oliverbisbee@gmail.com>
2025-07-22 21:17:22 -0400

    cleaning code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>StudySpots TO</h1>
+        <p>Find the perfect study spot around UofT</p>
       </header>
+      <main>
+        <div id="map-container" style={{ height: '500px', backgroundColor: '#f0f0f0', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <p>Map will go here</p>
+        </div>
+      </main>
     </div>
   );
 }
